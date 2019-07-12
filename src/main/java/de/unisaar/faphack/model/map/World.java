dfb5e1f2abebad911c747052e8943ed8f5b440a7
bkiefer <kiefer@dfki.de>
2019-07-12 16:42:33 +0200

    Move inhabitants field from World to Room. Closes #10.

--- src/main/java/de/unisaar/faphack/model/map/World.java
+++ src/main/java/de/unisaar/faphack/model/map/World.java
@@ -3,7 +3,6 @@ package de.unisaar.faphack.model.map;
 import java.util.List;
 
 import de.unisaar.faphack.model.Game;
-import de.unisaar.faphack.model.Character;
 
 /**
  * @author
@@ -13,8 +12,6 @@ public class World {
   private Game g;
 
   private List<Room> mapElements;
-
-  private List<Character> inhabitants;
 
   public World() {
 
