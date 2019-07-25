e27fb68dc0b97e13a8ad5ab6d057bd618958d58f
cwillms <christian.willms@dfki.de>
2019-07-25 13:53:10 +0200

    fixed gameTest.equip

--- src/test/java/de/unisaar/faphack/model/GameTest.java
+++ src/test/java/de/unisaar/faphack/model/GameTest.java
@@ -121,8 +121,6 @@ class GameTest {
     // Equip an armor
     Armor armor = createArmor(1,1,1);
     character.items.add(armor);
-    assertTrue(character.equipItem(armor));
-
     // the armor should be in the character's armor list
     assertTrue(game.equip(character, armor));
 
