50c98caf1497fd2f936f0f1e4cf08ed6b50582ff
weissenh <s8piweis@stud.uni-saarland.de>
2019-07-25 15:52:12 +0200

    Started with Item and its implementations
    Not finished yet

--- src/main/java/de/unisaar/faphack/model/Wearable.java
+++ src/main/java/de/unisaar/faphack/model/Wearable.java
@@ -18,7 +18,7 @@ public class Wearable extends Item {
   /**
    *
    */
-  protected boolean isWeapon;
+  protected boolean isWeapon = false;
 
   /**
    * The character who carries this item. This is null if the Item is placed on a
@@ -27,6 +27,25 @@ public class Wearable extends Item {
   protected Character character;
 
   public Wearable() {
+
+  }
+
+  public Wearable(Tile where, String trait, CharacterModifier effect, Character c) {
+    super(where, trait, effect);
+    this.character = c;
+    this.isWeapon = false;
+    String[] weapons = {POTION, SWORD, SHIELD, PIKE, BOW, ARROW, ARMOR};
+    for (String weapon: weapons) {
+      if (trait.equals(weapon)) this.isWeapon = true;
+    }
+    // todo have someone else tell me what are possible weapons: dangerous to do here: miss somehting
+//    static final String POTION = "potion";
+//    static final String SWORD = "sword";
+//    static final String SHIELD = "shield";
+//    static final String PIKE = "pike";
+//    static final String BOW = "bow";
+//    static final String ARROW = "arrow";
+//    static final String ARMOR = "armor";
 
   }
 
@@ -46,11 +65,52 @@ public class Wearable extends Item {
     this.character = c.read("character");
   }
 
+  public int getWeight() {
+    return weight;
+  }
+
+  /** @return the owner of the wearable, returns <code>null</code> if not owned by anyone*/
+  public Character getOwner() {
+    return character;
+  }
+
   public void pickUp(Character c) {
-    // TODO please implement me!
+    if (c == null) {
+      return;
+    }
+    if (getOwner() != null) {
+      // todo: what to do if want to pick up but owned by another person?
+      // do we need to test this?
+      return;
+    }
+    assert onTile != null; // if getOwner == null, tile should not be null
+    if (!onTile.equals(c.getTile())) {
+      return; // cannot pickup if not on same tile
+    }
+
+    boolean pickuppossible = c.pickUp(this);
+    if (pickuppossible) {
+      character = c;
+      onTile = null;
+    }
   }
 
   public void drop(Tile t) {
-    // TODO please implement me!
+    if (t == null) {
+      System.err.println("Wearable: drop to null??"); // debug
+    }
+    if (character == null) {
+      // can I drop an wearable if character is null?
+      // drop from one tile to another?
+      assert super.onTile != null;  // cannot teleport from one tile to another
+      return;
+    }
+    // todo [question] should I call character.dropItem(this)? ensure character drops item?
+    // would maybe create loop, don't do it?
+    // violates OOP because it depends on implementation of Chararcter.dropItem()
+    // todo [question] can I drop wearables to walltiles (but destructible?)? doortiles, stairtiles....?
+    super.onTile = t;
+    character = null;
+    return;
   }
 }
