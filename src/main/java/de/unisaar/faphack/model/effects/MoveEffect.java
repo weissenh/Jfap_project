963ddf7e45b84cdcd53d82f09d9d67cae466bfc9
Fabian Gössl <s8fagoes@stud.uni-saarland.de>
2019-07-25 13:28:20 +0200

    Move effect finished. Test passed.

--- src/main/java/de/unisaar/faphack/model/effects/MoveEffect.java
+++ src/main/java/de/unisaar/faphack/model/effects/MoveEffect.java
@@ -12,6 +12,11 @@ public class MoveEffect implements Effect<Character, Boolean> {
 
   public MoveEffect(Direction d) {
     dir = d;
+
+    //Checks whether the move exceeds the distance limitations
+    if (abs(this.dir.x)>1 || abs(this.dir.y)>1) {  //
+      throw new IllegalArgumentException("Move exceeds distance limitations.");
+    }
   }
 
   /**
@@ -28,18 +33,26 @@ public class MoveEffect implements Effect<Character, Boolean> {
   public Boolean apply(Character c) {
     // todo: check whether we only move one tile
     // todo: question: what if destination tile contains a trap?
-    if (abs(this.dir.x) >= 1 && abs(this.dir.y) >= 1) {  // allows also diagonal move
-      return false;
-    }
+
     Tile cpos = c.getTile(); // current tile: starting point of character
     // ask the room the tile is in where we will end up if we move in the direction
     Room currentroom = cpos.getRoom();
     Tile desttile = currentroom.getNextTile(cpos, dir);  // maybe not full direction
     // ask the destination tile whether it will take the character (answer null if cannot be moved)
+
     Tile t = desttile.willTake(c);
     if (t == null) {
       return false;
     }
+
+    //Verifies whether the character still has power left, returns false if not
+    if (c.getPower() == 0) {
+      if (this.dir.x == 0 && this.dir.y == 0) {
+        //does not need to move as c is already on correct tile
+        c.rest(); //increases characters power by 5
+      } else return false;
+    }
+
     c.move(desttile);
     return true;
   }
--- src/test/java/de/unisaar/faphack/model/effects/MoveEffectTest.java
+++ src/test/java/de/unisaar/faphack/model/effects/MoveEffectTest.java
@@ -20,6 +20,9 @@ class MoveEffectTest {
    * 3. Direction (-1, 1) down (this should have now effect as there is a wall )
    * 4. Direction (0,2) exception expected as only directions with values from -1 to 1 are allowed in MoveEffects
    * 5. move actions are not allowed when the character's power is 0
+   *
+   * Possible further tests:
+   * todo: 6. destructible wall move*
    */
   @Test
   void apply() {
@@ -27,17 +30,21 @@ class MoveEffectTest {
     Tile[][] tiles = room.getTiles();
     Character testObject = createBaseCharacter("Foo", 2, 2);
     addCharacter(room, 1, 1, testObject);
+    //1.
     MoveEffect moveEffect = new MoveEffect(new Direction(0,1));
     moveEffect.apply(testObject);
     assertEquals(tiles[1][2], testObject.getTile());
+    //2.
     moveEffect = new MoveEffect(new Direction(0,-1));
     moveEffect.apply(testObject);
     assertEquals(tiles[1][1], testObject.getTile());
-    moveEffect = new MoveEffect(new Direction(-1,0));
+    //3.
+    moveEffect = new MoveEffect(new Direction(-1,0)); //should this not be -1 -1
     moveEffect.apply(testObject);
     assertEquals(tiles[1][1], testObject.getTile());
+    //4.
     assertThrows(IllegalArgumentException.class, () ->  new MoveEffect(new Direction(0, 2)));
-    // create a testObject with 0 power
+    //5. create a testObject with 0 power
     Character testObject2 = createBaseCharacter("Bar", 0, 2);
     addCharacter(room, 2,2, testObject2);
     moveEffect = new MoveEffect(new Direction(1,1));
