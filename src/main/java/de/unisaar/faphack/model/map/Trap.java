055a87845ba9e440a20f11bbf8f0a1d2810b3b17
weissenh <s8piweis@stud.uni-saarland.de>
2019-07-28 15:20:10 +0200

    added another constructor for trap

--- src/main/java/de/unisaar/faphack/model/map/Trap.java
+++ src/main/java/de/unisaar/faphack/model/map/Trap.java
@@ -8,30 +8,39 @@ import de.unisaar.faphack.model.MarshallingContext;
  * A Trap is a special Fixture. Its effect is triggered by moving on the tile it
  * is placed on.
  *
- * @author
+ * @author weissenh
  *
  */
 public class Trap extends Fixtures {
   /**
-   * Traps might also be placed on StairTiles. In this case, the stair is mas ked
+   * Traps might also be placed on StairTiles. In this case, the stair is masked
    * by the Trap and thus not visible for the character, i.e. a trap door.
    *
    */
   protected StairTile trapDoor = null;
 
-  protected CharacterModifier modifier;
+  // protected CharacterModifier modifier;
+  // not needed, Items (Trap is subsubclass of Item) already have a charactermodifier
+
+  public Trap(FloorTile where, StairTile trap, CharacterModifier effect) {
+    super(where, FLOOR, effect);
+    this.trapDoor = trap; // todo: stairtile ensure same room?
+  }
 
   public Trap() {
-
+    // trait = FLOOR; // all traps look like normal floor tiles? todo ok?
+    this(null, null, null);
   }
+
+  // todo: how to ensure that the trap effect is applied to character if he moves onto the tile?
 
   public void marshal(MarshallingContext c) {
     super.marshal(c);
-    c.write("trapDoor", this.trapDoor); // todo: what if null?
+    c.write("trapDoor", this.trapDoor);
   }
 
   public void unmarshal(MarshallingContext c) {
     super.unmarshal(c);
-    this.trapDoor = c.read("trapDoor"); // todo: what if null?
+    this.trapDoor = c.read("trapDoor");
   }
 }
