d0ad0cc616f1bc516c05bc0905adc0fb81a66575
weissenh <s8piweis@stud.uni-saarland.de>
2019-07-28 13:25:28 +0200

    Added comments to key class, deleted effect
    keys shouldn't have a character modifier effect, right?

--- src/main/java/de/unisaar/faphack/model/Key.java
+++ src/main/java/de/unisaar/faphack/model/Key.java
@@ -4,15 +4,21 @@ import de.unisaar.faphack.model.effects.ItemEffect;
 import de.unisaar.faphack.model.MarshallingContext;
 import de.unisaar.faphack.model.map.Tile;
 
+/**
+ * Keys are used to unlock doors.
+ * To unlock a door a character has to have a key with a matching keyID.
+ * Keys don't have character modifier effects
+ */
 public class Key extends Wearable implements Storable {
-
+  /**
+   * To unlock a door, you need a key with a matching keyID
+   * */
   private int keyID;
 
   //todo: Fab better method to get trait into the constructor
-  public Key(Tile where, CharacterModifier effect, Character c, int kid) {
-    super(where, KEY, effect, c);
-    keyID = kid;
-
+  public Key(Tile where, Character c, int kid) {
+    super(where, KEY, null, c);
+    this.keyID = kid;
   }
 
   public int getKeyID() {
@@ -20,15 +26,13 @@ public class Key extends Wearable implements Storable {
   }
 
   @Override
-
   public void marshal(MarshallingContext c) {
     super.marshal(c);
     // c.write("stair", this.stair);
     c.write("keyID", this.keyID);
   }
 
-  //this.stair = c.read("stair");
-
+  @Override
   public void unmarshal(MarshallingContext c) {
     super.unmarshal(c);
     this.keyID = c.readInt("keyID");
