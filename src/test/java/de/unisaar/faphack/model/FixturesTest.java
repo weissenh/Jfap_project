4259437b0433610fdf4fde3dcb96e0eb9c46927a
weissenh <s8piweis@stud.uni-saarland.de>
2019-07-28 15:21:22 +0200

    added fixture test (fails right now)
    if a character moves onto a tile with a fountain (fixtures), the fountain effect (healing) should be applied to the character

---
+++ src/test/java/de/unisaar/faphack/model/FixturesTest.java
@@ -0,0 +1,31 @@
+package de.unisaar.faphack.model;
+
+import de.unisaar.faphack.model.map.FloorTile;
+import static de.unisaar.faphack.model.TestUtils.*;
+
+import de.unisaar.faphack.model.map.Room;
+import de.unisaar.faphack.model.map.Tile;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FixturesTest {
+  // todo also test chest?
+  @Test
+  void testFountain() {
+    CharacterModifier effect = new CharacterModifier(1, 0, 0, 1);
+    Room room = createSimpleRoom(4,4,1);
+    FloorTile tile = new FloorTile(1, 1, room);
+    Fixtures fountain = new Fixtures(tile, TraitOwner.FOUNTAIN, effect);
+    // test proper initialization
+    assertEquals(fountain.getTile(), tile);
+    assertEquals(fountain.getTrait(), TraitOwner.FOUNTAIN);
+    assertEquals(fountain.getCharacterModifier(), effect);
+    Character person = createBaseCharacter("person1", 2, 10);
+    assertEquals(person.getHealth(), 100); // todo value might change (100)
+    person.move(tile);
+    assertEquals(person.getTile(), tile);
+    // todo see effect of fountain?
+    assertEquals(person.getHealth(), 101);
+  }
+}
