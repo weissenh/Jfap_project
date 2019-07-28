66bb941f2498c40cf37e9f899ec3696f9262618a
weissenh <s8piweis@stud.uni-saarland.de>
2019-07-28 15:18:48 +0200

    Added another constructor for Fixtures

--- src/main/java/de/unisaar/faphack/model/Fixtures.java
+++ src/main/java/de/unisaar/faphack/model/Fixtures.java
@@ -1,5 +1,7 @@
 package de.unisaar.faphack.model;
 
+import de.unisaar.faphack.model.map.FloorTile;
+
 /**
  * Fixtures are Items that can't be carried by a Character. They are placed on a
  * Tile and the Character can interact with them through actions (healing fount
@@ -11,9 +13,18 @@ package de.unisaar.faphack.model;
 public class Fixtures extends Item {
 
   public Fixtures() {
-    super(null, "chest", null);
+    super(null, FOUNTAIN, null);
+    // super(null, CHEST, null);
     // todo: what kind of effect?
     // todo: what to do with trait? chest, fountain trait
   }
 
+  public Fixtures(FloorTile where, String trait, CharacterModifier effect) {
+    // assert (trait != null);   // todo: don't do input validation with assert
+    // assert (FOUNTAIN.equals(trait) || CHEST.equals(trait));
+    super(where, trait, effect);
+  }
+  // todo: do anything special with chests to allow interaction with them?
+  // e.g. chest don't have character modifier but
+  // if you interact with them new wearables may appear on the current tile (but can interact only once?)
 }
